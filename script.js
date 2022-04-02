60802050c1b4eea32bc1abe223bb99e97fdcb33e
Lueks <lueksse@gmail.com>
2022-04-02 11:26:03 +0200

    Moved styles over to styles.css and the script to script.js, added a color picker.

--- script.js
+++ script.js
@@ -4,22 +4,38 @@ function $(id) {
 const grid = $("grid");
 const column = $("column");
 const box = $("box");
+const clrPck = $("clrPck");
+
 
 let clmns = prompt("How many columns?", 16) -1;
 let rws = prompt("How many rows?", 16) -1;
 
 for (let i = 0; i <= clmns; i++) {
-let newColumn = document.createElement("div");
-newColumn.setAttribute("class", "column");
-grid.appendChild(newColumn);
-for (let j = 0; j <= rws; j++) {
-    let newBox = document.createElement("div");
-    newBox.setAttribute("class", "box");
-    newColumn.appendChild(newBox);        
+    let newColumn = document.createElement("div");
+    newColumn.setAttribute("class", "column");
+    grid.appendChild(newColumn);
+        for (let j = 0; j <= rws; j++) {
+            let newBox = document.createElement("div");
+            newBox.setAttribute("class", "box");
+            newColumn.appendChild(newBox);        
+            };
 };
-}
+
+let currentColor = clrPck.value; 
+clrPck.addEventListener("change", () => {
+    currentColor = clrPck.value;
+});
+
+let test = () => {console.log(currentColor)};
 
 let btns = document.querySelectorAll("div.box");
+btns.forEach((btn) => {
+btn.addEventListener("click", (e) => {
+    e.target.style.backgroundColor = currentColor;   
+});
+
+// Implements the "paint" and "remove-paint" functionality to every div with the class .box
+/* let btns = document.querySelectorAll("div.box");
 btns.forEach((btn) => {
 btn.addEventListener("click", (e) => {
     if (!e.target.classList.contains("boxC")) {
@@ -28,13 +44,14 @@ btn.addEventListener("click", (e) => {
     else {
         e.target.classList.remove("boxC");
     };   
-});
+}); */
+
 
 let rstBtn = $("resetBtn");
 rstBtn.addEventListener("click", () => {
-btns.forEach((btn) => {
-    btn.classList.remove("boxC");
-});
-}); 
+    btns.forEach((btn) => {
+        btn.style.backgroundColor = "#ffffff";
+        });
+    }); 
 
 });
